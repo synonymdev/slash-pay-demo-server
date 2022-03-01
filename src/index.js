06295adf418975bc777d988d0c14ed835f002c01
Corey Phillips <corey.lyle.phillips@gmail.com>
2022-03-01 12:37:05 +0000

    Updated subscribeToInvoice & subscribeToAddress methods. Added p2sh & p2pkh to SUPPORTED_METHODS. Added p2sh & p2pkh to the runMethod method switch statement. Added data param to runMethod method.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { slashtagsPayServer } from 'hackday-slashpay';
 import { readFileSync } from 'fs';
 import * as lns from 'ln-service';
-import {once} from 'events';
 import { v4 as uuidv4 } from 'uuid';
 
 const CERT = '/Users/user/.polar/networks/1/volumes/lnd/alice/tls.cert';
@@ -42,53 +41,54 @@ const generateAddress = async (format = 'p2wpkh') => {
 }
 
 const subscribeToInvoice = async (invoiceIdHexString, callback) => {
-    //TODO: This is a hack until we can properly await the invoice subscription.
     const sub = lns.subscribeToInvoice({id: invoiceIdHexString, lnd});
-    const [invoice] = await once(sub, 'invoice_updated');
-    await new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, 5000);
+    sub.on('invoice_updated', (data) => {
+        //TODO: Ensure the proper amount has been received.
+        if (data?.received > 0) {
+            callback({ orderId: uuidv4(), error: !data, data: data?.id });
+            sub.abort();
+        }
     });
-    callback({ orderId: uuidv4(), error: !invoice, data: invoice.id });
 };
 
 const subscribeToAddress = async (address = '', addressType = 'bech32', callback) => {
-    //TODO: This is a hack until we can properly listen for an address.
-    await new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, 5000);
+    if (addressType === 'p2wpkh') addressType = 'bech32';
+    const sub = lns.subscribeToChainAddress({
+        lnd,
+        [`${addressType}_address`]: address,
+        min_height: 1,
+        min_confirmations: 0
     });
-    callback({ orderId: uuidv4(), error: false, data: 'txid...' });
-    /*return new Promise((resolve) => {
-        if (addressType === 'p2wpkh') addressType = 'bech32';
-        const sub = lns.subscribeToChainAddress({lnd, [`${addressType}_address`]: address});
-        sub.on('confirmation', (data) => {
-            callback({ orderId: uuidv4(), error: data.error, data });
-        });
-    });*/
+    sub.on('confirmation', (data) => {
+        callback({ orderId: uuidv4(), error: !data, data: data?.transaction });
+        sub.abort();
+    });
 }
 
-const SUPPORTED_METHODS = ['bolt11', 'p2wpkh'];
+const SUPPORTED_METHODS = ['bolt11', 'p2wpkh', 'p2sh', 'p2pkh' ];
 const methodIsSupported = (method) => SUPPORTED_METHODS.includes(method);
 const getSupportedMethods = (methods) => methods.filter((method) => methodIsSupported(method));
 
-const runMethod = async (method) => {
-    let data = { error: true, data: 'No supported method is available.', id: '' };
-    if (!method) return data;
+const runMethod = async (method, data) => {
+    let response = { error: true, data: 'No supported method is available.', id: '' };
+    if (!method) return response;
     switch (method) {
         case 'bolt11':
-            data = await generateInvoice({ tokens: data.amount, description: data.description });
+            response = await generateInvoice({ tokens: Number(data.amount), description: data.description });
             break;
         case 'p2wpkh':
-            data = await generateAddress(method);
+        case 'p2sh':
+        case 'p2pkh':
+            response = await generateAddress(method);
             break;
         default:
             break;
 
     }
-    return { method, ...data };
+    console.log('\n');
+    console.log('Receipt Response:', { method, ...response });
+    console.log('\n');
+    return { method, ...response };
 }
 
 const runSubscribe = async (method, id, callback) => {
@@ -110,7 +110,7 @@ const lnNodeResponse = async (data, onInvoice, onReceipt) => {
     // Grab an array of supported methods, if any.
     const supportedMethods = getSupportedMethods(data.methods);
     // Attempt to run the first supported method. Returns error if undefined.
-    const methodResponse = await runMethod(supportedMethods[0]);
+    const methodResponse = await runMethod(supportedMethods[0], data);
     onInvoice(methodResponse);
     if (!methodResponse.error) {
         await runSubscribe(methodResponse.method, methodResponse.id, onReceipt);
